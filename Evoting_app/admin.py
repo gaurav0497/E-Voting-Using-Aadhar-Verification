5230ee33542075cf63b66afa2e611145bd84b62f
gaurav0497 <thakurgaurav497@gmail.com>
2021-03-21 16:32:46 +0530

    Added voter database

--- Evoting_app/admin.py
+++ Evoting_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Candidate
+from .models import Candidate,Voter
 admin.site.register(Candidate)
+admin.site.register(Voter)
--- Evoting_app/models.py
+++ Evoting_app/models.py
@@ -14,3 +14,16 @@ class Candidate(models.Model):
 
     def __str__(self):
         return self.name
+
+class Voter(models.Model):
+    id = models.AutoField
+    name = models.CharField(max_length=50)
+    district = models.CharField(max_length=50, default="")
+    gender = models.CharField(max_length=50,default="")
+    mobile = models.IntegerField()
+    aadhar_number = models.CharField(max_length=12)
+    register_date = models.DateField(default=datetime.now())
+    dob = models.DateField()
+
+    def __str__(self):
+        return self.name
