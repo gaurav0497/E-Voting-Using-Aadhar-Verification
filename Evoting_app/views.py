e2423b3878757662e5b055b21882eea95e9903b3
gaurav0497 <thakurgaurav497@gmail.com>
2021-03-27 02:18:05 +0530

    added backend

--- Evoting_app/views.py
+++ Evoting_app/views.py
@@ -5,4 +5,12 @@ def index(request):
     return render(request,'index.html')
 
 def register(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        mobile = request.POST.get('mobile')
+        state = request.POST.get('state')
+        date = request.POST.get('date')
+        sex = request.POST.get('sex')
+        print(name, mobile, state, date, sex)
+
     return render(request,'register.html')
