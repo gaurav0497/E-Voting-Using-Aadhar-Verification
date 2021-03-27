130c60d5ca042f6d90315a73e447060cc00486fd
gaurav0497 <thakurgaurav497@gmail.com>
2021-03-27 20:54:25 +0530

    updated design base

--- static/js/register.js
+++ static/js/register.js
@@ -13,9 +13,9 @@ signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
 });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
+// signInButton.addEventListener('click', () => {
+// 	container.classList.remove("right-panel-active");
+// });
 
 name.addEventListener('input',()=>{
 	
@@ -28,3 +28,35 @@ mobile.addEventListener('input',()=>{
 state.addEventListener('input',()=>{
 	console.log("Hello");
 });
+
+/* Capture Code*/
+function startcamera()
+{
+	var player = document.getElementById('player');
+	var snapshotCanvas = document.getElementById('snapshot');
+	var captureButton = document.getElementById('capture');
+
+	var x=document.getElementById('cam');
+	x.style.display="flex";
+
+	var handleSuccess = function(stream) {
+	  // Attach the video stream to the video element and autoplay.
+	  player.srcObject = stream;
+	};
+  
+	captureButton.addEventListener('click', function() {
+	  var context = snapshot.getContext('2d');
+	  // Draw the video frame to the canvas.
+	  context.drawImage(player, 0, 0, snapshotCanvas.width,
+		  snapshotCanvas.height);
+	//   console.log(context.canvas.toDataURL());
+	var fullQuality = snapshotCanvas.toDataURL('image/jpeg', 1.0);
+	
+	document.getElementById('image').href=fullQuality;
+	console.log();
+	});
+
+	navigator.mediaDevices.getUserMedia({video: true})
+		.then(handleSuccess);
+	  
+}
